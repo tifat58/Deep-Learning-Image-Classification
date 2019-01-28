84e09a1b6e2382bd926149d3ee986eaec20b539b
Mak-Ta-Reque <maktareq@gmail.come>
2019-01-28 12:57:03 +0100

    modules added

--- DataViz.py
+++ DataViz.py
@@ -10,18 +10,19 @@ class DataVizulaize:
     def rgb2gray(self,rgb):
         return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
 
-    def vizualize(self,layer_num, num_kernel,gray=False, gaus_filter=True,sigma = 1 ):
+    def vizualize(self,layer_num, num_kernel,gray=False, gaus_filter=True,sigma = 1,shapeofgrid=(16,8) ):
         if num_kernel%2>0:
             im_h = 8
             im_w = num_kernel+1/2
         else:
             im_h = 8
             im_w = num_kernel/2
+            
         #for layer in model.layers:
         #weights = layer.get_weights() # list of numpy arrays
         kernals = self.model.layers[layer_num].get_weights()[0]
         #np.shape(kernals)
-        fig, axs = plt.subplots(im_h,im_w, figsize=(15,10))
+        fig, axs = plt.subplots(shapeofgrid[0],shapeofgrid[1], figsize=(15,10))
         fig.subplots_adjust(hspace = .01, wspace=.01)
         axs = axs.ravel()
         for i in range(np.shape(kernals)[3]):
@@ -32,7 +33,7 @@ class DataVizulaize:
             one_kernal += 128
             one_kernal= np.clip(one_kernal, 0, 255).astype('uint8')
             if(gray):
-                one_kernal =rgb2gray(one_kernal)
+                one_kernal =self.rgb2gray(one_kernal)
                 
             if gaus_filter:
                 one_kernal = gaussian_filter(one_kernal, sigma)
